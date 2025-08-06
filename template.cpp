63528307664b068aa91a1c67345aae0c5aa7c0b9
Young143 <young143@yeah.net>
2025-08-06 15:21:52 +0800

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+//比赛默认模板
+#include <bits/stdc++.h>
+#define endl "\n"
+#define rep(i, a, b) for (i64 i = a; i <= b; i++)
+#define lep(i, a, b) for (i64 i = a; i >= b; i--)
+// #define MOD
+using namespace std;
+using i64 = int64_t;
+using ui64 = uint64_t;
+using vi64 = vector<i64>;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+
+    return 0;
+}
